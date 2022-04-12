c84a4dd475c39b43ca9c937f4ba11cc9cb9c6ba9
JhojaniSilvestre <maellysilvestre@gmail.com>
2022-04-12 20:09:46 +0200

    ejercicio 1 completado
    se añaden los terminos ordenados por orden ascendente a una tabla con un contador de las veces que aparece

--- js/practica-03.js
+++ js/practica-03.js
@@ -25,16 +25,7 @@ function anyadirDefinicion(){
 		let lista = document.getElementById("lista");
 		//obtengo todos los nodos dt
 		let todos = lista.getElementsByTagName("dt"); //devuelve un array
-		/*-------tabla ------------*/
 
-		//obtengo el nodo de la tabla
-		let tabla = document.getElementById("tabla");
-		//obtengo el nodo del tbody
-		let tbody = tabla.querySelector("tbody");
-		//obtengo una coleccion con todos los nodos tr
-		let filas = tbody.getElementsByTagName("tr");
-
-		/*-------------------------*/
 		let ausente=true; //para comprobar si la palabra se repite
 		let indice=0;
 		
@@ -90,7 +81,48 @@ function anyadirDefinicion(){
 			//se añaden al final automaticamente (por eso primero añadimos el nodo dt y luego dd)
 			lista.appendChild(neWord); 
 			lista.appendChild(newDef);
+		}
 
+		/*-------tabla --------------------------------------------------------------------------------------*/
+
+		//obtengo el nodo de la tabla
+		let tabla = document.getElementById("tabla");
+		//obtengo el nodo del tbody
+		let tbody = tabla.querySelector("tbody");
+		//obtengo una coleccion con todos los nodos tr
+		let filas = tbody.getElementsByTagName("tr");
+
+		ausente=true;
+		indice=0;
+		while (ausente && indice < filas.length) {
+			let celdas=filas.item(indice).getElementsByTagName("td");
+			if (palabra == celdas.item(0).textContent) {
+				ausente = false;
+				let valorCeldaNum = parseInt(celdas.item(1).textContent,10);
+				let resultado = valorCeldaNum + 1;
+				celdas.item(1).textContent=resultado;
+			}
+			else if (palabra < celdas.item(0).textContent) {
+				ausente = false;
+				//creo los nuevos nodos de la tabla
+				let newFila=document.createElement("tr");
+				let newCeldaWord=document.createElement("td");
+				let newCeldaCont=document.createElement("td");
+				//creo los nodos de texto
+				let textWordTable=document.createTextNode(palabra);
+				let textWordNum=document.createTextNode("1");
+				//asigno los nodos texto a los nodos celda correspondientes
+				newCeldaWord.appendChild(textWordTable);
+				newCeldaCont.appendChild(textWordNum);
+				//asigno los nodos celda al nodo fila
+				newFila.appendChild(newCeldaWord);
+				newFila.appendChild(newCeldaCont);
+
+				tbody.insertBefore(newFila, filas.item(indice));
+			}
+			indice++;
+		}
+		if (ausente) {
 			//creo los nuevos nodos de la tabla
 			let newFila=document.createElement("tr");
 			let newCeldaWord=document.createElement("td");
@@ -106,17 +138,6 @@ function anyadirDefinicion(){
 			newFila.appendChild(newCeldaCont);
 			//asigno el nodo fila al nodo tbody
 			tbody.appendChild(newFila);
-		}
-		else{
-			for (let i = 0; i < filas.length; i++) {
-				let celdas=filas.item(i).getElementsByTagName("td");
-				if (palabra == celdas.item(0).textContent) {
-					let valorCeldaNum = parseInt(celdas.item(1).textContent,10);
-					let resultado = valorCeldaNum + 1;
-					celdas.item(1).textContent=resultado;
-				}
-				
-			}
 		}
     }
 }
