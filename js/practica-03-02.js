635e11e8bd9cec0056db52d419cd944f058cad98
JhojaniSilvestre <maellysilvestre@gmail.com>
2022-04-14 20:41:25 +0200

    ejercicio 2 completo
    borra todas las provincias de la select provincias

--- js/practica-03-02.js
+++ js/practica-03-02.js
@@ -28,73 +28,75 @@ function mostrarProvincias(){
     let cb = "Santander";
     let ri = "Logroño";
     let ib = "Palma de Mallorca";
-    let an = Array("Almería","Cádiz","Córdoba","Granada","Huelva","Jaén","Málaga","Sevilla");
-    let ar = Array("Huesca","Teruel","Zaragoza");
-    let cn = Array("Santa Cruz de Tenerife","La Palmas de Gran Canaria");
-    let cm = Array("Albacete","Ciudad Real","Cuenca","Guadalajara","Toledo");
-    let cl = Array("Ávila","Burgos","León","Salamanca","Segovia","Soria","Valladolid","Zamora");
-    let ct = Array("Barcelona","Gerona","Lérida","Tarragona");
-    let vc = Array("Alicante","Castellón de la Plana","Valencia");
-    let ex = Array("Badajoz","Cáceres");
-    let ga = Array("La Coruña","Lugo","Orense","Pontevedra");
-    let pv = Array("Bilbao","San Sebastián","Vitoria");
+    let an = new Array("Almería","Cádiz","Córdoba","Granada","Huelva","Jaén","Málaga","Sevilla");
+    let ar = new Array("Huesca","Teruel","Zaragoza");
+    let cn = new Array("Santa Cruz de Tenerife","La Palmas de Gran Canaria");
+    let cm = new Array("Albacete","Ciudad Real","Cuenca","Guadalajara","Toledo");
+    let cl = new Array("Ávila","Burgos","León","Salamanca","Segovia","Soria","Valladolid","Zamora");
+    let ct = new Array("Barcelona","Gerona","Lérida","Tarragona");
+    let vc = new Array("Alicante","Castellón de la Plana","Valencia");
+    let ex = new Array("Badajoz","Cáceres");
+    let ga = new Array("La Coruña","Lugo","Orense","Pontevedra");
+    let pv = new Array("Bilbao","San Sebastián","Vitoria");
 
     //compruebo el valor seleccionado para así crear los nodos correspondientes
     if (value_autonom == "md") {
-        mostrarProvincia(md);
+        una_provincia(md);
     }
     else if (value_autonom == "mc") {
-        mostrarProvincia(mc);
+        una_provincia(mc);
     }
     else if (value_autonom == "as") {
-        mostrarProvincia(as);
+        una_provincia(as);
     }
     else if (value_autonom == "nc") {
-        mostrarProvincia(nc);
+        una_provincia(nc);
     }
     else if (value_autonom == "cb") {
-        mostrarProvincia(cb);
+        una_provincia(cb);
     }
     else if (value_autonom == "ri") {
-        mostrarProvincia(ri);
+        una_provincia(ri);
     }
     else if (value_autonom == "ib") {
-        mostrarProvincia(ib);
+        una_provincia(ib);
     }
     else if (value_autonom == "an") {
-        mostrarProvincia(an);
+        varias_provincias(an);
     }
-    else if (value_autonom == "as") {
-        mostrarProvincia(as);
+    else if (value_autonom == "ar") {
+        varias_provincias(ar);
     }
-    else if (value_autonom == "as") {
-        mostrarProvincia(as);
+    else if (value_autonom == "cn") {
+        varias_provincias(cn);
     }
-    else if (value_autonom == "as") {
-        mostrarProvincia(as);
+    else if (value_autonom == "cm") {
+        varias_provincias(cm);
     }
-    else if (value_autonom == "as") {
-        mostrarProvincia(as);
+    else if (value_autonom == "cl") {
+        varias_provincias(cl);
     }
-    else if (value_autonom == "as") {
-        mostrarProvincia(as);
+    else if (value_autonom == "ct") {
+        varias_provincias(ct);
     }
-    else if (value_autonom == "as") {
-        mostrarProvincia(as);
+    else if (value_autonom == "vc") {
+        varias_provincias(vc);
     }
-
-
-    
-
-
-
-
+    else if (value_autonom == "ex") {
+        varias_provincias(ex);
+    }
+    else if (value_autonom == "ga") {
+        varias_provincias(ga);
+    }
+    else if (value_autonom == "pv") {
+        varias_provincias(pv);
+    }
 }
 
-function mostrarProvincia(provincia){
+function una_provincia(provincia){
     //obtengo el nodo de la select de comunidades autonomas
-	let provincias = document.getElementById("provincias");
-
+	let select = document.getElementById("provincias");
+    select = borrar_options(select);
     //creo el nuevo nodo option de la select
     let newOption = document.createElement("option");
 	//creo los nodos de texto
@@ -102,6 +104,50 @@ function mostrarProvincia(provincia){
     //asigno los nodos texto a los nodos option correspondientes
 	newOption.appendChild(textOption);
     //asigno el nodo option al nodo select
-	provincias.appendChild(newOption);
+	select.appendChild(newOption);
 
 }
+
+
+function varias_provincias(provincias){
+    //obtengo el nodo de la select de comunidades autonomas
+	let select = document.getElementById("provincias");
+    select = borrar_options(select);
+    for (let index = 0; index < provincias.length; index++) {
+        //creo el nuevo nodo option de la select
+        let newOption = document.createElement("option");
+        //creo los nodos de texto
+        let textOption=document.createTextNode(provincias[index]);
+        //asigno los nodos texto a los nodos option correspondientes
+        newOption.appendChild(textOption);
+        //asigno el nodo option al nodo select
+        select.appendChild(newOption);
+    }
+}
+
+function borrar_options(select){
+    //obtengo una coleccion con todos los nodos
+	/*let options = select.getElementsByTagName("option");
+    
+    pos = 0;
+    //alert(options.length);
+    if (options.length > 0) {
+        while (pos < options.length) {
+            select.removeChild(options.item(pos));
+            pos++;
+        }
+    }
+*/
+//comprueba que mientras existe al menos un nodo hijo
+    while (select.firstChild) {
+        //borrara todos los nodos hijos
+        select.removeChild(select.firstChild)
+    }
+    //devuelve la select sin nodos hijos
+    return select;  
+}
+
+
+
+
+
